c1e2095518d7070e07c10cf5c8ef598b0f215395
1337kid <45532566+1337kid@users.noreply.github.com>
2023-09-09 03:44:03 -0400

    v0.5.1

--- fvico.sh
+++ fvico.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+# colours
 white="\033[1;37m"
 purple="\033[0;35m"
 green="\033[1;32m"
 cyan="\033[0;36m"
 
+# useragent => You can replace this if you want :)
 useragent='Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/114.0'
 
 echo -e "$purple
@@ -16,15 +18,16 @@ echo -e "$purple
  #         # #    #  #     # #     # 
  #          #    ###  #####  #######
 
- 	@1337kid       v0.5"
+ 	@1337kid       v0.5.1"
 echo
 
+# CURL installation checking
 if [[ $(which curl) == '' ]]
 then
-	echo -e "\033[0;31m[ERROR] CURL is not installed"
-	exit
+	echo -e "\033[0;31m[ERROR] CURL is not installed" && exit
 fi
 
+# disaply usage
 if [[ $1 == '' ]]
 then
 	echo -e '\033[0;36m
@@ -37,13 +40,14 @@ fvico https://exampleweb.tld/favicon.ico
 	exit
 fi
 
+# checking the host
 check_url=$(curl -s -o /dev/null -w "%{http_code}" $1)
-if [[ $check_url == '000' ]]
-then
-	echo -e "\033[0;31m[ERROR] Error" && exit
-elif [[ $check_url == '404' ]]
+if [[ $check_url == '404' ]]
 then
 	echo -e "\033[0;31m[ERROR 404] Favicon not found" && exit
+elif [[ $check_url != '200' ]]
+then
+	echo -e "\033[0;31m[ERROR] Error" && exit
 fi
 
 echo -e "$green[ URL ] :$cyan $1"
