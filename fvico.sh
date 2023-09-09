842976fb2d9df1e21d0dd7936dba1d3ec773d7eb
1337kid <45532566+1337kid@users.noreply.github.com>
2023-09-09 03:44:02 -0400

    v0.3

--- fvico.sh
+++ fvico.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 
+white="\033[1;37m"
+purple="\033[0;35m"
+green="\033[1;32m"
+cyan="\033[0;36m"
+
 useragent='Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/114.0'
 
+echo -e "$purple
+ ####### #     # ###  #####  ####### 
+ #       #     #  #  #     # #     # 
+ #       #     #  #  #       #     # 
+ #####   #     #  #  #       #     # 
+ #        #   #   #  #       #     # 
+ #         # #    #  #     # #     # 
+ #          #    ###  #####  #######
+ 	@1337kid       v0.3         "
+echo
+echo -e "$green[ URL ] :$cyan $1"
 md5=$(curl $1 -A $useragent -s | md5sum)
+echo -e "$green[ MD5 SUM ] :$cyan $md5"
+echo -e "$purple[ INFO ] :$white Checking with OWASP favicon database"
 data=$(curl https://wiki.owasp.org/index.php/OWASP_favicon_database -A $useragent -s | grep [a-f0-9]:[a-z] | grep $md5)
 IFS=':' read -ra result <<< $data
-echo MD5 SUM   : ${result[0]}
-echo Framework : ${result[1]}
+
+echo -e "$green[ Framework ] :$cyan ${result[1]}$white"
